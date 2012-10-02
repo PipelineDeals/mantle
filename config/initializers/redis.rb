61ad8e315d25271259bf056f2b236acf28c1d106
Christopher David Yudichak <TheOddLinguist@gmail.com>
2012-10-02 00:46:04 -0400

    a little cleanup

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,44 +1,47 @@
 require 'redis'
 
-##
-# Configure Redis
-##
+class RedisRunner
+  def initialize
+    $redis = Redis.new#(:host => 'topic-staging.pipelinedealsco.com')
+    @namespace = 'jupiter'
+    @handler = ModelHandler.new
+  end
 
-$redis = Redis.new
-# $redis = Redis.new(:host => 'topic-staging.pipelinedealsco.com')
+  def run!
+    catchup
+    start_listener
+  end
 
-namespace = "jupiter"
-handler = ModelHandler.new
+  private
 
-##
-# Lookup missed messages
-##
+  def catchup
+    keys = $redis.keys('jupiter:action_list:*')
+    keys.each do |key|
+      ns, list, timestamp, model, action, id = key.split(':')
+      if timestamp.to_f > 1349147313.266263 # TODO Change to dynamically saved value
+        channel = "#{@namespace}:#{action}:#{model}"
+        message = $redis.get key
+        @handler.call(channel, message)
+      end
+    end
+  end
 
-keys = $redis.keys('jupiter:action_list:*')
-keys.each do |key|
-  ns, list, timestamp, model, action, id = key.split(':')
-  if timestamp.to_f > 1349147313.266263 # TODO Change to dynamically saved value
-    channel = "#{namespace}:#{action}:#{model}"
-    message = $redis.get key
-    handler.call(channel, message)
+  def start_listener
+    models = %w{person contact lead company deal note}
+    actions = %w{create update destroy}
+
+    channels = []
+    models.each do |model|
+      actions.each do |action|
+        channels << "#{action}:#{model}"
+      end
+    end
+
+    # TODO Didn't work
+    # channels = models.map { |model| actions.map { |action| "#{action}:#{model}" } }
+
+    Subscriber.new(@namespace, channels, @handler).listen
   end
 end
 
-##
-# Setup service for new incoming messages
-##
-
-models = %w{person contact lead company deal note}
-actions = %w{create update destroy}
-
-channels = []
-models.each do |model|
-  actions.each do |action|
-    channels << "#{action}:#{model}"
-  end
-end
-
-# TODO Didn't work
-# channels = models.map { |model| actions.map { |action| "#{action}:#{model}" } }
-
-Subscriber.new(namespace, channels, handler).listen
+RedisRunner.new.run!
