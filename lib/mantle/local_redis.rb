858eb14c9bb9b2a6c885dd4b6a5d1c3834a1559e
Brandon Hilkert <brandonhilkert@gmail.com>
2013-07-31 15:59:38 -0400

    Be explicit about class methods

--- lib/mantle/local_redis.rb
+++ lib/mantle/local_redis.rb
@@ -1,22 +1,20 @@
 module Mantle
   class LocalRedis
-    class << self
-      def set_message_successfully_received
-        Sidekiq.redis do |conn|
-          conn.set('last_successful_message_received', Time.now.to_i)
-        end
+    def self.set_message_successfully_received
+      Sidekiq.redis do |conn|
+        conn.set('last_successful_message_received', Time.now.to_i)
       end
+    end
 
-      def last_message_successfully_received_at
-        Sidekiq.redis do |conn|
-          conn.get('last_successful_message_received')
-        end
+    def self.last_message_successfully_received_at
+      Sidekiq.redis do |conn|
+        conn.get('last_successful_message_received')
       end
+    end
 
-      def get(key)
-        Sidekiq.redis do |conn|
-          conn.get key
-        end
+    def self.get(key)
+      Sidekiq.redis do |conn|
+        conn.get key
       end
     end
   end
