3935b0e6bea3dc11e0fdaf1b506cb8ceee79260a
Brandon Hilkert <brandonhilkert@gmail.com>
2013-08-01 12:03:47 -0400

    Remove setup

--- lib/mantle/tasks.rb
+++ lib/mantle/tasks.rb
@@ -6,7 +6,6 @@ require 'mantle/sidekiq_overrides'
 namespace :mantle do
   desc "Runs the listener to listen for changes to things in PipelineDeals"
   task :listen do
-    Mantle.setup
     Mantle.run!
   end
 
