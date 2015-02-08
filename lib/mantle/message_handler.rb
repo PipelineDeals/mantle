389e79b3688de9a0b98df1a64dc8b4d026dbb29c
Brandon Hilkert <brandonhilkert@gmail.com>
2015-02-07 19:50:10 -0500

    Fix order of arguments for handler

--- lib/mantle/message_handler.rb
+++ lib/mantle/message_handler.rb
@@ -1,7 +1,7 @@
 module Mantle
   class MessageHandler
     def self.receive(model, action, message)
-      raise Mantle::Error::MissingImplementation.new("Implement self.receive(action, model, object) and assign class to the message handler")
+      raise Mantle::Error::MissingImplementation.new("Implement self.receive(model, action, object) and assign class to the message handler")
     end
   end
 end
