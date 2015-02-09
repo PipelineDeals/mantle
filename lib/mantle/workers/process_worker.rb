fd9c6beb84fb3db23c628ff2c1d7c43bf4a82dc2
Brandon Hilkert <brandonhilkert@gmail.com>
2015-02-09 15:21:40 -0500

    Enqueue cleanup on message receipt

--- lib/mantle/workers/process_worker.rb
+++ lib/mantle/workers/process_worker.rb
@@ -8,6 +8,7 @@ module Mantle
       def perform(channel, message)
         Mantle.receive_message(channel, message)
         Mantle::LocalRedis.set_message_successfully_received
+        Mantle::CatchUp.new.enqueue_clear_if_ready
       end
     end
   end
--- spec/lib/mantle/workers/process_worker_spec.rb
+++ spec/lib/mantle/workers/process_worker_spec.rb
@@ -14,6 +14,12 @@ describe Mantle::Workers::ProcessWorker do
       expect(Mantle::LocalRedis).to receive(:set_message_successfully_received){ true }
       Mantle::Workers::ProcessWorker.new.perform("person:update", message)
     end
+
+    it "runs the expire clean on catch up" do
+      allow(Mantle).to receive_messages(receive_message: true)
+      expect_any_instance_of(Mantle::CatchUp).to receive(:enqueue_clear_if_ready)
+      Mantle::Workers::ProcessWorker.new.perform("person:update", message)
+    end
   end
 end
 
